25f293ca32031abf08494f4967c5684df4ed9f0e
Mamzky <auliya77@gmail.com>
2023-12-16 13:38:12 +0700

    [feat] Update stock, customer

---
+++ src/Customer/CustomerDetail.js
@@ -0,0 +1,320 @@
+import React, { useEffect, useState } from 'react'
+import { Button, CloseButton, Col, Form, Modal, Row, Spinner } from 'react-bootstrap'
+import TopNavBar from '../Components/TopNavBar'
+import SideNavBar from '../Components/SideNavBar'
+import SmallImageCard from '../Components/SmallImageCard'
+import CustomTable from '../Components/CustomTable'
+import { useNavigate, useParams } from 'react-router-dom'
+import { DigitFormatter, OnlyDigit } from '../Utils/General'
+import { empty, isEmpty } from 'ramda'
+import { db } from '../Config/FirebaseConfig';
+import { collection, getDocs, addDoc, doc, updateDoc, query, where } from 'firebase/firestore'
+import moment from 'moment/moment'
+import { CUSTOMER_COLLECTION, LOG_COLLECTION, PRODUCT_COLLECTION } from '../Utils/DataUtils'
+import Constant from '../Utils/Constants'
+import { addLog } from '../Utils/Utils'
+import AppColors from '../Utils/Colors'
+
+function CustomerDetail() {
+
+    const navigate = useNavigate()
+
+    const [customerName, setCustomerName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [address, setAddress] = useState('')
+    const [contactPerson, setContactPerson] = useState('')
+    const [notes, setNotes] = useState('')
+
+    const [showModal, setShowModal] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    // validationFlag
+    const [errorName, setErrorName] = useState('')
+    const [errNameExist, setErrNameExist] = useState(false)
+    const [errorSize, setErrorSize] = useState('')
+    const [errorBasePrice, setErrorBasePrice] = useState('')
+    const [errorSellPrice, setErrorSellPrice] = useState('')
+    const [errorSupplier, setErrorSupplier] = useState('')
+    const [errorQty, setErrorQty] = useState('')
+    const { customer_code } = useParams()
+
+
+    // NEW STATE
+    const [customerData, setCustomerData] = useState()
+    const [isEdit, setIsEdit] = useState(false)
+
+
+    const summaryItem = (title, value) => {
+        return (
+            <div className='mb-2'>
+                <span style={{ fontWeight: 'bold' }}>{title}</span><br />
+                <span style={{ margin: '10px' }}>{value}</span>
+            </div>
+        )
+    }
+
+    const validation = () => {
+        setShowModal(true)
+    }
+
+    useEffect(() => {
+        getCustomerData(customer_code)
+    }, [])
+
+    const getCustomerData = async (customer_code) => {
+        setIsLoading(true)
+        const q = query(collection(db, CUSTOMER_COLLECTION)
+            , where('customer_code', '==', customer_code))
+        const querySnapshot = await getDocs(q)
+        const result = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        if (!querySnapshot.empty) {
+            setCustomerData(result[0])
+            console.log('RESULT', result[0]);
+            setIsLoading(false)
+        } else {
+            setIsLoading(false)
+            console.log('FAILED');
+        }
+    }
+
+    const loadDataToForm = (data) => {
+        setCustomerName(data?.name)
+        setPhone(data?.phone)
+        setEmail(data?.email)
+        setAddress(data?.address)
+        setContactPerson(data?.contact_person)
+        setNotes(data?.notes)
+    }
+
+    const updateDataCustomer = async () => {
+        setIsLoading(true)
+        const newCustomerData = {
+            name: customerName,
+            phone: phone,
+            email: email,
+            address: address,
+            contact_person: contactPerson,
+            notes: notes,
+            updated_at: moment(new Date).toISOString(),
+            customer_code: customerData?.customer_code,
+            updated_at: moment().format('DD/MMM/YYYY hh:mm')
+        }
+        const oldProductDoc = doc(db, CUSTOMER_COLLECTION, customerData?.id)
+        updateDoc(oldProductDoc, newCustomerData)
+            .then(() => {
+                setIsLoading(false)
+                setShowModal(false)
+                addLog('UPDATE CUSTOMER DATA', `${localStorage.getItem(Constant.USERNAME)} update customer ${customerName}, from ${JSON.stringify(customerData)} to ${JSON.stringify({
+                    name: customerName,
+                    phone: phone,
+                    email: email,
+                    address: address,
+                    contact_person: contactPerson,
+                    notes: notes,
+                })}`)
+                navigate('/customer')
+            })
+    }
+
+    return (
+        <div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <Modal.Header>
+                    <Modal.Title>
+                        Ubah Data Customer
+                    </Modal.Title>
+                    <CloseButton onClick={() => setShowModal(false)} />
+                </Modal.Header>
+                <Modal.Body>
+                    {summaryItem('Nama customer', customerName)}
+                    {summaryItem('Nomor telepon', phone)}
+                    {summaryItem('Email', email)}
+                    {summaryItem('Alamat', address)}
+                    {summaryItem('Contact person', contactPerson)}
+                    {summaryItem('Catatan', notes)}
+                </Modal.Body>
+                <Modal.Footer>
+                    <p style={{ width: '100%', textAlign: 'center' }}>Apakah data yang dimasukkan sudah benar?</p>
+                    <Button variant="danger" onClick={() => setShowModal(false)}>Batal</Button>
+                    <Button variant="success" onClick={() => { updateDataCustomer() }}>Ya, Ubah</Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={isLoading} centered>
+
+                <Modal.Body backdrop={'false'} show={true} onHide={() => setShowModal(false)}
+                    size="md"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    style={{
+                        display: 'flex',
+                        flex: 1,
+                        flexDirection: 'row',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <div>
+                        <Spinner animation="border" role="status" style={{ alignSelf: 'center' }}>
+                        </Spinner>
+                    </div>
+                    <h3 style={{ marginLeft: 20 }}>Loading...</h3>
+                </Modal.Body>
+            </Modal>
+            <SideNavBar />
+            <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+                <TopNavBar />
+                <div className="container-fluid py-4">
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div className="col-lg-6 col-md-3 mb-md-0 mb-4">
+                            <h2>{customerData?.name}</h2>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <Form>
+                            {/* NAME */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='productName'>
+                                <Form.Label>Nama Customer</Form.Label>
+                                {isEdit ?
+                                    <Form.Control
+                                        // isInvalid={errorName}
+                                        type="input"
+                                        name='customerName'
+                                        value={customerName}
+                                        onChange={(e) => {
+                                            setCustomerName(e.target.value)
+                                        }}
+                                        placeholder="Masukan nama"
+                                    />
+                                    :
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{customerData?.name}</h4>
+                                }
+                            </Form.Group>
+
+                            {/* PHONE */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='phone'>
+                                <Form.Label>Nomor Telepon Customer</Form.Label>
+                                {isEdit ?
+                                    <Form.Control
+                                        // isInvalid={errorName}
+                                        type="input"
+                                        name='customerPhone'
+                                        value={phone}
+                                        onChange={(e) => {
+                                            setPhone(e.target.value)
+                                        }}
+                                        placeholder="Masukan nomor telepon"
+                                    />
+                                    :
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{customerData?.phone}</h4>
+                                }
+                            </Form.Group>
+
+                            {/* EMAIL */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='email'>
+                                <Form.Label>Email Customer</Form.Label>
+                                {isEdit ?
+                                    <Form.Control
+                                        // isInvalid={errorName}
+                                        type="input"
+                                        name='customerEmail'
+                                        value={email}
+                                        onChange={(e) => {
+                                            setEmail(e.target.value)
+                                        }}
+                                        placeholder="Masukan email"
+                                    />
+                                    :
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{customerData?.email}</h4>
+                                }
+                            </Form.Group>
+
+                            {/* ADDRESS */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='address'>
+                                <Form.Label>Alamat Customer</Form.Label>
+                                {isEdit ?
+                                    <Form.Control
+                                        // isInvalid={errorName}
+                                        type="input"
+                                        as="textarea" rows={3}
+                                        name='customerAddress'
+                                        value={address}
+                                        onChange={(e) => {
+                                            setAddress(e.target.value)
+                                        }}
+                                        placeholder="Masukan alamat"
+                                    />
+                                    :
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{customerData?.address}</h4>
+                                }
+                            </Form.Group>
+
+                            {/* CONTACT PERSON */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='contactPerson'>
+                                <Form.Label>Contact Person</Form.Label>
+                                {isEdit ?
+                                    <Form.Control
+                                        // isInvalid={errorName}
+                                        type="input"
+                                        name='contactPerson'
+                                        value={contactPerson}
+                                        onChange={(e) => {
+                                            setContactPerson(e.target.value)
+                                        }}
+                                        placeholder="Contact person"
+                                    />
+                                    :
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{customerData?.contact_person}</h4>
+                                }
+                            </Form.Group>
+
+                            {/* NOTES */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='notes'>
+                                <Form.Label>Catatan</Form.Label>
+                                {isEdit ?
+                                    <Form.Control
+                                        // isInvalid={errorName}
+                                        type="input"
+                                        as="textarea" rows={3}
+                                        name='cuctomerNotes'
+                                        value={notes}
+                                        onChange={(e) => {
+                                            setNotes(e.target.value)
+                                        }}
+                                        placeholder="Catatan"
+                                    />
+                                    :
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{customerData?.notes}</h4>
+                                }
+                            </Form.Group>
+
+                            <div className="col-lg-8 col-md-3 my-4" style={{ display: 'flex', flex: 1, flexDirection: 'row' }}>
+                                <Button
+                                    variant='light'
+                                    style={{ width: '25%', alignSelf: 'flex-start', marginRight: 20 }}
+                                    onClick={() => {
+                                        isEdit ? setIsEdit(false) : navigate(-1)
+                                    }}
+                                >{isEdit ? 'Batal' : 'Kembali'}</Button>
+                                <Button
+                                    style={{ width: '25%', alignSelf: 'flex-end' }}
+                                    onClick={() => {
+                                        if (isEdit) {
+                                            validation()
+                                        } else {
+                                            loadDataToForm(customerData)
+                                            setIsEdit(true)
+                                        }
+                                    }}
+                                >{isEdit ? 'Simpan' : 'Edit Data Customer'}</Button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default CustomerDetail
--- src/Stock/Stock.js
+++ src/Stock/Stock.js
@@ -1,13 +1,137 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SideNavBar from '../Components/SideNavBar'
 import TopNavBar from '../Components/TopNavBar'
-import SmallImageCard from '../Components/SmallImageCard'
-import { Button } from 'react-bootstrap'
-import CustomTable from '../Components/CustomTable'
+import { Button, Modal, Form } from 'react-bootstrap'
+import { db } from '../Config/FirebaseConfig';
+import { PRODUCT_COLLECTION } from '../Utils/DataUtils'
+import { collection, doc, getDocs, query, updateDoc } from 'firebase/firestore'
+import { OnlyDigit } from '../Utils/General'
+import { addLog } from '../Utils/Utils';
+import Constant from '../Utils/Constants';
+import moment from 'moment';
 
 function Stock() {
+
+  const productCollectionRef = collection(db, PRODUCT_COLLECTION)
+  const [loading, setLoading] = useState(false)
+  const [stock, setStock] = useState([])
+  const [modalUpdateStock, setModalUpdateStock] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState('')
+  const [newStock, setNewStock] = useState(0)
+  const [reason, setReason] = useState('')
+
+  const getStock = async () => {
+    const data = await getDocs(productCollectionRef)
+    const sortedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    setStock(sortedData.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)))
+  }
+
+  useEffect(() => {
+    getStock()
+  }, [])
+
+  const updateStock = async () => {
+    console.log(selectedProduct)
+    const productHolder = { ...selectedProduct }
+    productHolder.qty = newStock
+    productHolder.latest_update = reason
+    productHolder.updated_at = moment().format('DD/MMM/YYYY hh:mm')
+    console.log(productHolder)
+
+    const producDoc = doc(db, PRODUCT_COLLECTION, selectedProduct.id)
+    await updateDoc(producDoc, productHolder).then((res) => {
+      addLog('UPDATE STOCK', `${localStorage.getItem(Constant.USERNAME)} update stock "${selectedProduct?.product_name}" dari ${selectedProduct?.qty} menjadi ${newStock}`)
+      getStock()
+      setModalUpdateStock(false)
+      setLoading(false)
+      setNewStock(0)
+    })
+  }
+
+  const searchProduct = async (productName) => {
+    const q = query(collection(db, PRODUCT_COLLECTION.toString()))
+    const querySnapshot = await getDocs(q);
+    const result = querySnapshot.docs
+      .map(doc => doc.data())
+      .filter((e) => e.product_name.toLowerCase()
+        .includes(productName.toLowerCase())).sort((a, b) => a.qty - b.qty)
+    setStock(result)
+  }
+
+
+
   return (
     <div>
+      <Modal
+        show={modalUpdateStock}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title>
+            Update Stok
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <h3 style={{ marginBottom: 40, textAlign: 'center' }}>{selectedProduct?.product_name ?? '-'}</h3>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div style={{ flex: 1 }}>
+                <h6 style={{ textAlign: 'center' }}>Jumlah stok sebelum</h6>
+                <div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                  <h4 style={{ textAlign: 'center', marginTop: -20 }}>{selectedProduct?.qty}</h4>
+                </div>
+              </div>
+              <div style={{ flex: 1 }}>
+                <h6 style={{ textAlign: 'center' }}>Jumlah stok terbaru</h6>
+                <Form.Group>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      flexDirection: 'row'
+                    }}
+                  >
+                    <Form.Control
+                      style={{
+                        width: '30%',
+                        fontSize: 24
+                      }}
+                      type='input'
+                      name='newStock'
+                      value={newStock}
+                      onChange={(e) => {
+                        const digitOnly = OnlyDigit(e.target.value)
+                        setNewStock(digitOnly)
+                      }}
+                    />
+                  </div>
+                </Form.Group>
+              </div>
+
+            </div>
+            <div>
+              <h6 style={{ textAlign: 'center' }}>Keterangan<p>{'(Wajib diisi)'}</p></h6>
+              <Form.Control
+                style={{
+                  width: '100%'
+                }}
+                type='input'
+                name='reason'
+                value={reason}
+                onChange={(e) => {
+                  setReason(e.target.value)
+                }}
+              />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={() => setModalUpdateStock(false)}>Batal</Button>
+          <Button disabled={reason === ''} variant="success" onClick={() => updateStock()}>Ya, Update</Button>
+        </Modal.Footer>
+      </Modal>
       <SideNavBar />
       <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <TopNavBar />
@@ -18,14 +142,98 @@ function Stock() {
               <h6>Daftar Stok JAA Alkesum</h6>
             </div>
           </div>
-          <div class="row mt-4">
+          {/* <div class="row mt-4">
             <SmallImageCard />
             <SmallImageCard />
             <SmallImageCard />
             <SmallImageCard />
+          </div> */}
+
+          <div>
+            <Form.Group className="col-lg-6 col-md-3" controlId='productName'>
+              <Form.Control
+                isInvalid={false}
+                type="input"
+                name='productName'
+                onChange={(e) => {
+                  setTimeout(() => {
+                    searchProduct(e.target.value)
+                  }, 500);
+                  // setProductName(e.target.value)
+                  // setErrorName(isEmpty(e.target.value))
+                }}
+                placeholder="Cari produk"
+              />
+            </Form.Group>
           </div>
           <div class="row mt-4">
-            <CustomTable />
+            <div className="col-lg-12 col-md-6 mb-md-0 mb-4">
+              <div className="card">
+                <div className="card-header pb-0">
+                  <div className="row">
+                    <div className="col-lg-6 col-7">
+                      <h6>{`Total Data: ${stock.length}`}</h6>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="card-body px-0 pb-2">
+                  <div className="table-responsive">
+                    <table className="table align-items-center mb-0">
+                      <thead>
+                        <tr>
+                          <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
+                          <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Produk</th>
+                          <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" style={{ textAlign: 'center' }}>Jumlah Stock</th>
+                          <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {stock?.map((item, index) => {
+                          return (
+                            <tr>
+                              <td>
+                                <div className="ps-3 py-1">
+                                  <div className="d-flex flex-column justify-content-center">
+                                    <h6 className="mb-0 text-sm">{index + 1}</h6>
+                                  </div>
+                                </div>
+                              </td>
+                              <td>
+                                <div className="ps-3 py-1">
+                                  <div className="d-flex flex-column justify-content-center">
+                                    <h6 className="mb-0 text-sm">{item?.product_name}</h6>
+                                  </div>
+                                </div>
+                              </td>
+                              <td>
+                                <div className="ps-3 py-1">
+                                  <div className="d-flex flex-column">
+                                    <h6 className="mb-0 text-sm" style={{ textAlign: 'center' }}>{item?.qty}</h6>
+                                  </div>
+                                </div>
+                              </td>
+                              <td>
+                                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+                                  <Button
+                                    style={{ width: '40%', marginTop: 10 }}
+                                    onClick={() => {
+                                      // navigate('/product/add-new-product')
+                                      setSelectedProduct(item)
+                                      setModalUpdateStock(true)
+                                    }}
+                                  >Update Stok</Button>
+                                </div>
+                              </td>
+                            </tr>
+                          )
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
