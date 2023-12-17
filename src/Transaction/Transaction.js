11b50b9a5983b863f80c13ae0ea36fb86e0578bc
Mamzky <auliya77@gmail.com>
2023-12-17 15:38:47 +0700

    [Fix] live feeback

--- src/Transaction/Transaction.js
+++ src/Transaction/Transaction.js
@@ -2,30 +2,60 @@ import React, { useEffect, useState } from 'react'
 import SideNavBar from '../Components/SideNavBar'
 import TopNavBar from '../Components/TopNavBar'
 import CustomTable from '../Components/CustomTable'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Dropdown, Form, Row } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { CUSTOMER_COLLECTION, TRANSACTION_COLLECTION } from '../Utils/DataUtils'
 import { db } from '../Config/FirebaseConfig';
 import { collection, getDocs, addDoc, doc, updateDoc, query, where } from 'firebase/firestore'
 import moment from 'moment'
 import { calculateTotal } from '../Utils/Utils'
-import { DigitFormatter } from '../Utils/General'
+import { DigitFormatter, PaymentTypeList, StatusTypeList, filterByList } from '../Utils/General'
+import { isEmpty } from 'ramda'
+import Select from 'react-select'
 
 function Transaction() {
 
   const navigate = useNavigate()
   const transactionCollectionRef = collection(db, TRANSACTION_COLLECTION)
+  const customerCollectionRef = collection(db, CUSTOMER_COLLECTION)
+
   const [transactionData, setTransactionData] = useState([])
+  const [customerList, setCustomerList] = useState([])
+  const [productList, setProductList] = useState([])
+
+  // FILTER
+  const [filterField, setFilterField] = useState('')
 
   const getTransactionList = async () => {
     const data = await getDocs(transactionCollectionRef)
     const sortedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-    console.log(sortedData.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
     setTransactionData(sortedData.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)))
+  }
+
+  const getCustomer = async () => {
+    const data = await getDocs(customerCollectionRef)
+    const sortedData = data.docs.map((doc) => ({ id: doc.id, label: `${doc.data().name}(${doc.data().contact_person})`, value: doc.data() }))
+    setCustomerList(sortedData.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)))
   }
   useEffect(() => {
     getTransactionList()
+    getCustomer()
   }, [])
+
+  const filterByOrderNumber = async (order_number) => {
+    const q = query(collection(db, TRANSACTION_COLLECTION)
+      , where('order_number', '==', order_number))
+    const querySnapshot = await getDocs(q)
+    const result = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+    setTransactionData(result)
+
+    // if (!querySnapshot.empty) {
+    //   setTransactionData(result)
+    //   console.log('RESULT', result[0]);
+    // } else {
+    //   console.log('FAILED');
+    // }
+  }
 
   // const searchCustomer = async (customerName) => {
   //   const q = query(collection(db, CUSTOMER_COLLECTION.toString()))
@@ -38,6 +68,79 @@ function Transaction() {
   //       .includes(customerName.toLowerCase()))
   //   setTransactionData(result)
   // }
+
+
+
+  const searchElement = () => {
+    switch (filterField) {
+      case 'Nomor Transaksi':
+        return (
+          <Form.Control
+            isInvalid={false}
+            type="input"
+            name='filterTransaction'
+            onChange={(e) => {
+              setTimeout(() => {
+                filterByOrderNumber(e.target.value)
+              }, 500);
+            }}
+            placeholder="Pencarian..."
+          />
+        )
+        break;
+      case 'Customer':
+        return (
+          <Select
+            options={customerList}
+            placeholder="Pilih customer"
+            onChange={(e) => {
+              // setSelectedCustomer(e.id)
+            }}
+          />
+        )
+        break;
+      case 'Jenis':
+        return (
+          <Select
+            options={PaymentTypeList}
+            placeholder="Jenis Pembayaran"
+            onChange={(e) => {
+              // setSelectedCustomer(e.id)
+            }}
+          />
+        )
+        break;
+      case 'Tanggal':
+        return (
+          <Form.Control
+            type="date"
+            // name='orderDate'
+            // value={orderDate}
+            onChange={(e) => {
+              // setOrderDate(e.target.value)
+            }}
+            placeholder="Tanggal order"
+          />
+        )
+        break;
+      case 'Status':
+        return (
+          <Select
+            options={StatusTypeList}
+            placeholder="Status Order"
+            onChange={(e) => {
+              console.log(e)
+            }}
+          />
+        )
+        break;
+      default:
+        return (
+          null
+        )
+        break;
+    }
+  }
 
   return (
     <div>
@@ -60,23 +163,26 @@ function Transaction() {
             </div>
           </div>
 
-          {/* <div>
-            <Form.Group className="col-lg-6 col-md-3" controlId='customerName'>
-              <Form.Control
-                isInvalid={false}
-                type="input"
-                name='customerName'
-                onChange={(e) => {
-                  setTimeout(() => {
-                    searchCustomer(e.target.value)
-                  }, 500);
-                  // setProductName(e.target.value)
-                  // setErrorName(isEmpty(e.target.value))
-                }}
-                placeholder="Cari customer"
-              />
+          <Row style={{ marginTop: 20, marginBottom: -20 }}>
+            <Dropdown className='col-lg-2'>
+              <Dropdown.Toggle className='col-lg-12' variant="success" id="dropdown-basic">
+                {isEmpty(filterField) ? 'Filter' : filterField}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {filterByList.map(({ label, value }) => {
+                  return (
+                    <Dropdown.Item
+                      onClick={() => setFilterField(label)}
+                    >{label}</Dropdown.Item>
+                  )
+                })}
+              </Dropdown.Menu>
+            </Dropdown>
+            <Form.Group className="col-lg-4 col-md-3" controlId='filterTransaction'>
+              {searchElement()}
             </Form.Group>
-          </div> */}
+          </Row>
 
           <div class="row mt-4">
             <div className="card-body px-0 pb-2">
@@ -107,7 +213,12 @@ function Transaction() {
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column justify-content-center">
-                                <h6 style={{cursor: 'pointer'}}  className="mb-0 text-sm">{item?.order_number}</h6>
+                                <h6
+                                  style={{ cursor: 'pointer' }}
+                                  onClick={() => {
+                                    navigate(`/transaction/detail/${item?.order_number}`)
+                                  }}
+                                  className="mb-0 text-sm">{item?.order_number}</h6>
                               </div>
                             </div>
                           </td>
@@ -128,7 +239,7 @@ function Transaction() {
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column">
-                                <h6 className="mb-0 text-sm">{'Aktif'}</h6>
+                                <h6 className="mb-0 text-sm">{item?.status ? 'Aktif' : 'Non-Aktif'}</h6>
                               </div>
                             </div>
                           </td>
---
+++ src/Transaction/TransactionDetail.js
@@ -0,0 +1,287 @@
+import React, { useEffect, useState } from 'react'
+import { Button, CloseButton, Col, Form, Modal, Row, Spinner } from 'react-bootstrap'
+import TopNavBar from '../Components/TopNavBar'
+import SideNavBar from '../Components/SideNavBar'
+import SmallImageCard from '../Components/SmallImageCard'
+import CustomTable from '../Components/CustomTable'
+import { useNavigate, useParams } from 'react-router-dom'
+import { DigitFormatter, OnlyDigit } from '../Utils/General'
+import { empty, isEmpty } from 'ramda'
+import { db } from '../Config/FirebaseConfig';
+import { collection, getDocs, addDoc, doc, updateDoc, query, where } from 'firebase/firestore'
+import moment from 'moment/moment'
+import { CUSTOMER_COLLECTION, LOG_COLLECTION, PRODUCT_COLLECTION, TRANSACTION_COLLECTION } from '../Utils/DataUtils'
+import Constant from '../Utils/Constants'
+import { addLog, calculateTotal } from '../Utils/Utils'
+import AppColors from '../Utils/Colors'
+
+function TransactionDetail() {
+
+    const navigate = useNavigate()
+
+    const [customerName, setCustomerName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [address, setAddress] = useState('')
+    const [contactPerson, setContactPerson] = useState('')
+    const [notes, setNotes] = useState('')
+
+    const [showModal, setShowModal] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    // validationFlag
+    const { order_number } = useParams()
+
+
+    // NEW STATE
+    const [orderData, setOrderData] = useState()
+    const [isEdit, setIsEdit] = useState(false)
+
+    // ORDER DATA
+    const [isActive, setIsActive] = useState(false)
+
+
+    const summaryItem = (title, value) => {
+        return (
+            <div className='mb-2'>
+                <span style={{ fontWeight: 'bold' }}>{title}</span><br />
+                <span style={{ margin: '10px' }}>{value}</span>
+            </div>
+        )
+    }
+
+    const validation = () => {
+        setShowModal(true)
+    }
+
+    useEffect(() => {
+        getOrderData(order_number)
+        setIsLoading(false)
+    }, [])
+
+    const getOrderData = async (order_number) => {
+        setIsLoading(true)
+        const q = query(collection(db, TRANSACTION_COLLECTION)
+            , where('order_number', '==', order_number))
+        const querySnapshot = await getDocs(q)
+        const result = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        if (!querySnapshot.empty) {
+            setOrderData(result[0])
+            console.log('RESULT', result[0]);
+            setIsLoading(false)
+        } else {
+            setIsLoading(false)
+            console.log('FAILED');
+        }
+    }
+
+    const getTransactionDetail = async (order_number) => {
+        console.log(order_number)
+    }
+
+    return (
+        <div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <Modal.Header>
+                    <Modal.Title>
+                        Ubah Data Customer
+                    </Modal.Title>
+                    <CloseButton onClick={() => setShowModal(false)} />
+                </Modal.Header>
+                <Modal.Body>
+                    {summaryItem('Nama customer', customerName)}
+                    {summaryItem('Nomor telepon', phone)}
+                    {summaryItem('Email', email)}
+                    {summaryItem('Alamat', address)}
+                    {summaryItem('Contact person', contactPerson)}
+                    {summaryItem('Catatan', notes)}
+                </Modal.Body>
+                <Modal.Footer>
+                    <p style={{ width: '100%', textAlign: 'center' }}>Apakah data yang dimasukkan sudah benar?</p>
+                    <Button variant="danger" onClick={() => setShowModal(false)}>Batal</Button>
+                    <Button variant="success" onClick={() => { }}>Ya, Ubah</Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={isLoading} centered>
+
+                <Modal.Body backdrop={'false'} show={true} onHide={() => setShowModal(false)}
+                    size="md"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    style={{
+                        display: 'flex',
+                        flex: 1,
+                        flexDirection: 'row',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <div>
+                        <Spinner animation="border" role="status" style={{ alignSelf: 'center' }}>
+                        </Spinner>
+                    </div>
+                    <h3 style={{ marginLeft: 20 }}>Loading...</h3>
+                </Modal.Body>
+            </Modal>
+            <SideNavBar />
+            <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+                <TopNavBar />
+                <div className="container-fluid py-4">
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div className="col-lg-6 col-md-3 mb-md-0 mb-4">
+
+                            <h2>
+                                <i
+                                    className="material-icons opacity-10"
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={() => navigate(-1)}
+                                >arrow_back_ios</i>
+                                {`Order Detail : ${order_number}`}</h2>
+                            <p style={{ paddingLeft: 24 }}>{`Dibuat pada : ${moment(orderData?.created_at).format('DD MMM YYYY hh:mm')}`}</p>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <Form>
+                            <Row>
+                                {/* NAME */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='productName'>
+                                    <Form.Label>Nama Customer</Form.Label>
+
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.customer?.name}</h4>
+
+                                </Form.Group>
+
+                                {/* PHONE */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='phone'>
+                                    <Form.Label>Nomor Telepon Customer</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.customer?.phone}</h4>
+                                </Form.Group>
+                            </Row>
+
+                            <Row>
+                                {/* EMAIL */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='email'>
+                                    <Form.Label>Email Customer</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.customer?.email}</h4>
+                                </Form.Group>
+
+                                {/* ADDRESS */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='address'>
+                                    <Form.Label>Alamat Customer</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.customer?.address}</h4>
+
+                                </Form.Group>
+                            </Row>
+
+                            <Row>
+                                {/* CONTACT PERSON */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='contactPerson'>
+                                    <Form.Label>Contact Person</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.customer?.contact_person}</h4>
+                                </Form.Group>
+
+                                {/* NOTES */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='notes'>
+                                    <Form.Label>Catatan</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{isEmpty(orderData?.notes) ? '-' : orderData?.notes}</h4>
+                                </Form.Group>
+                            </Row>
+                            <Row>
+                                {/* Tipe */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='contactPerson'>
+                                    <Form.Label>Jenis</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.type}</h4>
+                                </Form.Group>
+
+                                {/* NOTES */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='notes'>
+                                    <Form.Label>DP</Form.Label>
+                                    <h4 style={{ marginTop: -10, marginBottom: -10 }}>{orderData?.dp}</h4>
+                                </Form.Group>
+                            </Row>
+                            <Row>
+                                {/* Status */}
+                                <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='contactPerson'>
+                                    <Form.Label>Status</Form.Label>
+                                    <Form.Check // prettier-ignore
+                                        checked={isActive}
+                                        onChange={() => setIsActive((e) => !e)}
+                                        type="switch"
+                                        label={isActive ? 'Aktif' : 'Non-Aktif'}
+                                    />
+                                </Form.Group>
+                            </Row>
+                        </Form>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div className="card-body px-0 pb-2">
+                            <div className="table-responsive">
+                                <table className="table align-items-center mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
+                                            <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama Barang</th>
+                                            <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Jumlah</th>
+                                            <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Harga</th>
+                                            <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {orderData?.order_list?.map((item, index) => {
+                                            return (
+                                                <tr>
+                                                    <td>
+                                                        <div className="ps-3 py-1">
+                                                            <div className="d-flex flex-column justify-content-center">
+                                                                <h6 className="mb-0 text-sm">{index + 1}</h6>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="ps-3 py-1">
+                                                            <div className="d-flex flex-column justify-content-center">
+                                                                <h6 className="mb-0 text-sm">{item?.name}</h6>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="ps-3 py-1">
+                                                            <div className="d-flex flex-column">
+                                                                <h6 className="mb-0 text-sm">{item?.qty}</h6>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="ps-3 py-1">
+                                                            <div className="d-flex flex-column">
+                                                                <h6 className="mb-0 text-sm">{`Rp${DigitFormatter(item?.price)}`}</h6>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div className="ps-3 py-1">
+                                                            <div className="d-flex flex-column">
+                                                                <h6 className="mb-0 text-sm">{`Rp${DigitFormatter(item?.qty * item.price)}`}</h6>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+
+                                                </tr>
+                                            )
+                                        })}
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default TransactionDetail
