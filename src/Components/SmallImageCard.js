2f8c8ff0d73793a9b8e33259fc9971578d419bad
Mamzky <auliya77@gmail.com>
2024-01-01 17:42:35 +0700

    add data transaction

--- src/Components/SmallImageCard.js
+++ src/Components/SmallImageCard.js
@@ -1,21 +1,13 @@
 import React from 'react'
 
-function SmallImageCard() {
+function SmallImageCard({title, subtitle}) {
   return (
     <div class="col-xl-3 col-sm-6">
       <div class="card">
         <div class="card-header p-3 pt-2">
-          <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-            <i class="material-icons opacity-10">weekend</i>
-          </div>
           <div class="text-end pt-1">
-            <p class="text-sm mb-0 text-capitalize">Product Name</p>
-            <h4 class="mb-0">99</h4>
-          </div>
-        </div>
-        <div class="dark horizontal my-0">
-          <div class="card-footer p-3">
-            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than yesterday</p>
+            <h4 style={{textAlign:'left'}}>{title}</h4>
+            <h5 class="mb-0">{subtitle}</h5>
           </div>
         </div>
       </div>
