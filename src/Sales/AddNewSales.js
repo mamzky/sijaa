798ac7e29e8ca81573553b360a56344592491485
farhanmiqbal <farhanmiqbal10@gmail.com>
2024-04-04 11:34:33 +0700

    button diubah jadi 3 (batal, submit, edit)
    buat menu sales :
    no
    nama sales
    tujuan
    tanggal
    detail (button)
    
    tambahkan fitur search pada contact

---
+++ src/Sales/AddNewSales.js
@@ -0,0 +1,219 @@
+import SideNavBar from "../Components/SideNavBar"
+import TopNavBar from "../Components/TopNavBar"
+import { Button, CloseButton, Col, Form, Modal, Spinner } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { addDoc, collection } from "@firebase/firestore"
+import { db } from "../Config/FirebaseConfig"
+import { addLog } from "../Utils/Utils"
+import Constant from "../Utils/Constants"
+import { CONTACT_COLLECTION } from "../Utils/DataUtils"
+import { v4 as uuidv4 } from 'uuid';
+
+function AddNewSales() {
+
+    const navigate = useNavigate()
+
+    const [showModal, setShowModal] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const [formData, setFormData] = useState({
+        contactId: uuidv4(),
+        contactName: '',
+        contactPhone: '',
+        contactEmail: '',
+        contactAddress: '',
+        contactNotes: '',
+    });
+
+    const summaryItem = (title, value) => {
+        return (
+            <div className='mb-2'>
+                <span style={{ fontWeight: 'bold' }}>{title}</span><br />
+                <span style={{ margin: '10px' }}>{value}</span>
+            </div>
+        )
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+
+    const tampilpopup = () => {
+        // Melakukan sesuatu dengan data JSON, misalnya menampilkan di konsol
+        setShowModal(true);
+        // Jika Anda ingin melakukan sesuatu yang lain dengan data JSON, Anda bisa menambahkan kode di sini
+    };
+
+    const submitContact = async () => {
+
+        setFormData(prevState => ({
+            ...prevState,
+            ['contactId']: uuidv4()
+        }));
+
+        const contactCollectionRef = collection(db, CONTACT_COLLECTION)
+        await addDoc(contactCollectionRef, formData)
+            .then((res) => {
+                console.log(res);
+                addLog(localStorage.getItem(Constant.USERNAME), `create contact ${formData.contactName}`)
+                navigate('/contact')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }
+
+    return (
+        <div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <Modal.Header>
+                    <Modal.Title>
+                        Customer Baru
+                    </Modal.Title>
+                    <CloseButton onClick={() => setShowModal(false)} />
+                </Modal.Header>
+                <Modal.Body>
+                    {summaryItem('Nama customer', formData.contactName)}
+                    {summaryItem('Nomor telepon', formData.contactPhone)}
+                    {summaryItem('Email', formData.contactEmail)}
+                    {summaryItem('Alamat', formData.contactAddress)}
+                    {summaryItem('Catatan', formData.contactNotes)}
+                </Modal.Body>
+                <Modal.Footer>
+                    <p style={{ width: '100%', textAlign: 'center' }}>Apakah data yang dimasukkan sudah benar?</p>
+                    <Button variant="danger" onClick={() => setShowModal(false)}>Batal</Button>
+                    <Button variant="success" onClick={() => submitContact()}>Ya, Daftarkan</Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={isLoading} centered>
+
+                <Modal.Body backdrop={'false'} show={true} onHide={() => setShowModal(false)}
+                    size="md"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    style={{
+                        display: 'flex',
+                        flex: 1,
+                        flexDirection: 'row',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <div>
+                        <Spinner animation="border" role="status" style={{ alignSelf: 'center' }}>
+                        </Spinner>
+                    </div>
+                    <h3 style={{ marginLeft: 20 }}>Loading...</h3>
+                </Modal.Body>
+            </Modal>
+            <SideNavBar />
+            <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+                <TopNavBar />
+                <div className="container-fluid py-4">
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div className="col-lg-6 col-md-3 mb-md-0 mb-4">
+                            <h2>Tambah Sales</h2>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <Form>
+                            {/* NAME */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='contactName'>
+                                <Form.Label>Nama Sales</Form.Label>
+                                <Form.Control
+                                    type="input"
+                                    name='contactName'
+                                    value={formData.contactName}
+                                    onChange={handleInputChange}
+                                    placeholder="Masukan nama"
+                                />
+                            </Form.Group>
+
+                            {/* PHONE */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='phone'>
+                                <Form.Label>Tujuan</Form.Label>
+                                <Form.Control
+                                    type="input"
+                                    name='contactPhone'
+                                    id='phone'
+                                    value={formData.contactPhone}
+                                    onChange={(e) => {
+                                        if (/^\d*$/.test(e.target.value) && e.target.value.length < 14) {
+                                            handleInputChange(e)
+                                        }
+                                    }}
+                                    placeholder="Masukan Tujuan"
+                                />
+                            </Form.Group>
+
+                            {/* EMAIL */}
+                            <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='email'>
+                                <Form.Label>Tanggal</Form.Label>
+                                <Form.Control
+                                    type="input"
+                                    name='contactEmail'
+                                    id='email'
+                                    value={formData.contactEmail}
+                                    onChange={handleInputChange}
+                                    placeholder="Masukan Tanggal"
+                                />
+                            </Form.Group>
+
+                            {/* ADDRESS */}
+                            {/* <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='address'>
+                                <Form.Label>Alamat Contact</Form.Label>
+                                <Form.Control
+                                    type="input"
+                                    as="textarea" rows={3}
+                                    name='contactAddress'
+                                    id='email'
+                                    value={formData.contactAddress}
+                                    onChange={handleInputChange}
+                                    placeholder="Masukan alamat"
+                                />
+                            </Form.Group>
+
+                            {/* NOTES */}
+                            {/* <Form.Group className="col-lg-6 col-md-3" style={{ marginBottom: 20 }} controlId='notes'>
+                                <Form.Label>Catatan</Form.Label>
+                                <Form.Control
+                                    as="textarea" rows={3}
+                                    name='contactNotes'
+                                    id='email'
+                                    value={formData.contactNotes}
+                                    onChange={handleInputChange}
+                                    placeholder="Catatan"
+                                />
+                            </Form.Group> */} 
+
+
+                            <div className='my-4'>
+                                <Button
+                                    type='button'
+                                    variant='success'
+                                    style={{ width: '50%', alignSelf: 'flex-end' }}
+                                    onClick={() => {
+                                        tampilpopup()
+                                        console.log(formData)
+                                    }}
+                                >Tambah Sales Baru</Button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default AddNewSales
--- src/Sales/Sales.js
+++ src/Sales/Sales.js
@@ -6,42 +6,42 @@ import { Button, Form } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { collection, getDocs, query } from '@firebase/firestore'
 import { db } from '../Config/FirebaseConfig'
-import { CONTACT, CONTACT_COLLECTION } from '../Utils/DataUtils'
+import { SALES  } from '../Utils/DataUtils'
 
-function Contact() {
+function Sales() {
   const navigate = useNavigate()
   const [loading, setIsLoading] = useState(false)
-  const contactCollectionRef = collection(db, CONTACT_COLLECTION)
+//   const contactCollectionRef = collection(db, CONTACT_COLLECTION)
   const [contactData, setContactData] = useState([])
 
-  const getListContact = async () => {
-    setIsLoading(true)
-    await getDocs(contactCollectionRef)
-      .then((res) => {
-        const listData = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-        setContactData(listData)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
-  }
+//   const getListContact = async () => {
+//     setIsLoading(true)
+//     await getDocs(contactCollectionRef)
+//       .then((res) => {
+//         const listData = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+//         setContactData(listData)
+//       })
+//       .catch((err) => {
+//         console.log(err)
+//       })
+//       .finally(() => {
+//         setIsLoading(false)
+//       })
+//   }
 
-  useEffect(() => {
-    getListContact()
-  }, [])
+//   useEffect(() => {
+//     getListContact()
+//   }, [])
 
-  const searchContact = async (contactName) => {
-    const q = query(collection(db, CONTACT_COLLECTION.toString()))
-    const querySnapshot = await getDocs(q);
-    const result = querySnapshot.docs
-      .map(doc => doc.data())
-      .filter((e) => e.contactName.toLowerCase()
-        .includes(contactName.toLowerCase())).sort((a, b) => a.qty - b.qty)
-    setContactData(result)
-  }
+//   const searchContact = async (contactName) => {
+//     const q = query(collection(db, CONTACT_COLLECTION.toString()))
+//     const querySnapshot = await getDocs(q);
+//     const result = querySnapshot.docs
+//       .map(doc => doc.data())
+//       .filter((e) => e.contactName.toLowerCase()
+//         .includes(contactName.toLowerCase())).sort((a, b) => a.qty - b.qty)
+//     setContactData(result)
+//   }
 
   return (
     <div>
@@ -52,12 +52,12 @@ function Contact() {
           <div style={{ display: 'flex', flexDirection: 'row' }}>
             <div className="col-lg-6 col-md-3 mb-md-0 mb-4">
               <h2>Kontak</h2>
-              <h6>Daftar kontak JAA Alkesum</h6>
+              <h6>Daftar sales JAA Alkesum</h6>
             </div>
             <div className="col-lg-8 col-md-3" style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
               <Button style={{ width: '40%', alignSelf: 'flex-end' }}
                 onClick={() => {
-                  navigate('/contact/new-contact')
+                  navigate('/sales/new-sales')
                 }}
               >+ Tambah Kontak Baru</Button>
             </div>
@@ -69,14 +69,7 @@ function Contact() {
                 isInvalid={false}
                 type="input"
                 name='contactName'
-                onChange={(e) => {
-                  setTimeout(() => {
-                    searchContact(e.target.value)
-                  }, 500);
-                  // setProductName(e.target.value)
-                  // setErrorName(isEmpty(e.target.value))
-                }}
-                placeholder="Cari Kontak"
+                placeholder="Cari Sales"
               />
             </Form.Group>
           </div>
@@ -88,17 +81,17 @@ function Contact() {
                   <thead>
                     <tr>
                       <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
-                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama Contact</th>
-                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nomor Telepon</th>
-                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Email</th>
-                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Alamat</th>
+                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama Sales</th>
+                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tujuan</th>
+                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tanggal</th>
+                      <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Detail</th>
                     </tr>
                   </thead>
                   <tbody>
 
-                    {contactData.map((contact, index) => {
-                      return (
-                        <tr
+                    {/* {contactData.map((contact, index) => { */}
+                      {/* return ( */}
+                        {/* <tr
                           key={contact.contactId}
                           onClick={() => {
                             navigate(`/contact/detail/${contact.contactId}`)
@@ -106,45 +99,50 @@ function Contact() {
                           style={{ cursor: 'pointer' }} // Optional: Change cursor on hover
                           onMouseOver={(e) => (e.currentTarget.style.backgroundColor = 'black')}
                           onMouseOut={(e) => (e.currentTarget.style.backgroundColor = '')}
-                        >
+                        > */}
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column justify-content-center">
-                                <h6 className="mb-0 text-sm">{index + 1}</h6>
+                                <h6 className="mb-0 text-sm">1</h6>
                               </div>
                             </div>
                           </td>
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column justify-content-center">
-                                <h6 className="mb-0 text-sm">{contact.contactName}</h6>
+                                <h6 className="mb-0 text-sm">john doe</h6>
                               </div>
                             </div>
                           </td>
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column">
-                                <h6 className="mb-0 text-sm">{contact.contactPhone}</h6>
+                                <h6 className="mb-0 text-sm">lorem ipsum</h6>
                               </div>
                             </div>
                           </td>
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column">
-                                <h6 className="mb-0 text-sm">{contact.contactEmail}</h6>
+                                <h6 className="mb-0 text-sm">01/01/1999</h6>
                               </div>
                             </div>
                           </td>
                           <td>
                             <div className="ps-3 py-1">
                               <div className="d-flex flex-column">
-                                <h6 className="mb-0 text-sm">{contact.contactAddress}</h6>
+                                <h6 className="mb-0 text-sm"> <Button
+                                    style={{ width: '50%', alignSelf: 'flex-end' }}
+                                    onClick={() => {
+                                        navigate(`/sales/detail`)
+                                    }}
+                                >{'detail'}</Button></h6>
                               </div>
                             </div>
                           </td>
-                        </tr>
-                      )
-                    })}
+                        {/* </tr> */}
+                      
+                    
 
                   </tbody>
                 </table>
@@ -160,4 +158,4 @@ function Contact() {
 
 // 
 
-export default Contact
+export default Sales
